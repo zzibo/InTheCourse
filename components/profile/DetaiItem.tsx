4101f8e988d8f89cb274a6dbcbde46d75f5e51ae
Elisha-Loh <elishalohxx@gmail.com>
2025-01-18 23:22:17 +0800

    profile swipe

--- components/profile/DetaiItem.tsx
+++ components/profile/DetaiItem.tsx
@@ -2,10 +2,10 @@ import { View, Text } from "react-native";
 
 const DetailItem = ({ label, value }: { label: string; value: string }) => (
   <View className="flex flex-row items-center flex-wrap">
-    <Text className="text-lg font-semibold text-gray-700 dark:text-gray-300">
+    <Text className="text-lg font-semibold text-black dark:text-white">
       {label}
     </Text>
-    <Text className=" px-2 text-gray-600 dark:text-gray-400">{value}</Text>
+    <Text className=" px-2 text-black dark:text-white">{value}</Text>
   </View>
 );
 export default DetailItem;
